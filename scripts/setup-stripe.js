cc58e932bdb9bcec71c7b22c0f376ee270dba7c7
Anthony Simon <anthonyjcsa@gmail.com>
2026-03-31 17:46:32 -0500

    fix: corregir versión API de Stripe

--- scripts/setup-stripe.js
+++ scripts/setup-stripe.js
@@ -3,7 +3,7 @@ const Stripe = require('stripe')
 
 async function setupStripe() {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2024-11-20.acacia',
+    apiVersion: '2023-10-16',
   })
 
   console.log('🔧 Configurando productos en Stripe...\n')
